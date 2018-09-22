e6eb015548751fe2938cdd0ed53d409e55ede372
Fabio Dantas <dantasirl@hotmail.com>
2018-09-22 11:58:18 +0100

    Initial setup

---
+++ models/task.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var TaskSchema = new mongoose.Schema({
+
+    name: String,
+    deadline: String,
+    status: String,
+    description: String,
+    createdAt: {type: Date},
+});
+
+TaskSchema.pre('save', function(next){
+    now = new Date();
+    if (!this.createdAt) {
+        this.createdAt = now;
+    }
+    next();
+});
+
+
+module.exports = mongoose.model('users', TaskSchema);
